3fca6e362148f87589d4f0c4e0efa859aa5c2197
vitaly <vitalyryabchich@gmail.com>
2021-08-09 17:39:43 +0300

    creating app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react"
 function App() {
+  const [a, setA] = useState(0)
+
+  function increment() {
+    setA(a + 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increment}>Increment</button>
+      <button onClick={() => a -= 1}>decrement</button>
+      {a}
     </div>
   );
 }
